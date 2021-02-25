32c4eeca3eaa3a2b39e663f0612192e210a1baff
Ângelo Gabriel Lopes <79007043+Angelo-GL@users.noreply.github.com>
2021-02-25 09:14:58 -0300

    Estrutura inicial do projeto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 
+
+//Dentro desse arquivo ficarão os componentes fixos, ou seja, os componentes que estarão em todas as páginas.
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
