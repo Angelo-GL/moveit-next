74e5ff1fc3f71a4cbc8e1a4ef8ab6d931be93d2e
Ângelo Gabriel Lopes <79007043+Angelo-GL@users.noreply.github.com>
2021-02-25 15:28:06 -0300

    componente Countdown
    Implementação do componente Countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import {ExperienceBar} from '../components/ExperienceBar'
 import {Profile} from '../components/Profile'
+import {Countdown} from '../components/Countdown'
 
+import Head from 'next/head'
 
 import Styles from '../style/pages/Home.module.css'
+
 
 export default function Home() {
   return (
     <div className= {Styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges/>
+          <Countdown/>
         </div>
         <div>
 
