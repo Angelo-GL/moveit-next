1d2a0ee9916e910b4cc91211aa48178a41844d68
Ângelo Gabriel Lopes <79007043+Angelo-GL@users.noreply.github.com>
2021-02-25 10:44:33 -0300

    Estruturação do projeto

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-
-
-//Dentro desse arquivo ficarão os componentes fixos, ou seja, os componentes que estarão em todas as páginas.
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../style/global.css'
+//Dentro desse arquivo ficarão os componentes fixos, ou seja, os componentes que estarão em todas as páginas (componentes que se repetem).
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
